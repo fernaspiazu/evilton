998f127efba8b700eca17d978b96d3c8a20669a4
Fernando Aspiazu <fe.aspiazu@gmail.com>
2024-03-19 23:48:06 +0100

    hot reload in fastify app

--- apps/fleet-management/src/index.ts
+++ apps/fleet-management/src/index.ts
@@ -1,5 +1,31 @@
+import { FastifyInstance } from 'fastify';
+import Fastify from 'fastify';
+
+const fastifyApp = async () => {
+  const fastify: FastifyInstance = Fastify({
+    logger: true,
+  });
+
+  fastify.get('/health', async (_req, resp) => {
+    console.log('Healthy service');
+    resp.status(200).send({ message: 'Healthy service' });
+  });
+
+  return fastify;
+};
+
 async function main() {
-  console.log('Hello World');
+  const app = await fastifyApp();
+
+  try {
+    await app.listen({
+      host: '0.0.0.0',
+      port: 3000,
+    });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 }
 
 main();
